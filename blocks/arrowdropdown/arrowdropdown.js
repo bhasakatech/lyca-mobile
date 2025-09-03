81016de8df9403fd2f504c7fff1a5fc78759f4a6
divakarannareddy <divakarannareddy111@gmail.com>
2025-09-03 12:59:24 +0530

    arrowdropdown js build issue fixed

--- blocks/arrowdropdown/arrowdropdown.js
+++ blocks/arrowdropdown/arrowdropdown.js
@@ -1,12 +1,11 @@
 export default function decorate(block) {
-  const picture = block.querySelector("picture");
-  const img = picture?.querySelector("img"); 
-
+  const picture = block.querySelector('picture');
+  const img = picture?.querySelector('img');    
   if (img) {
-    img.addEventListener("click", function () {
+    img.addEventListener('click', () => {
       window.scrollTo({
         top: 0,
-        behavior: "smooth",
+        behavior: 'smooth',
       });
     });
   }
